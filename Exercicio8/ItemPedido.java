16568be7e9193a61d031fa1adb3c96aaecadd976
abevitor <blueprates86@gmail.com>
2026-09-25 11:02:02 -0300

    refatora a classe ItemPedido para incluir métodos de acesso e validação de quantidade

--- Exercicio8/ItemPedido.java
+++ Exercicio8/ItemPedido.java
@@ -2,6 +2,21 @@ package Exercicio8;
 
 public class ItemPedido {
     private Produto produto;
-    
-    
+    private int quantidade;
+
+    public ItemPedido(Produto produto, int quantidade) {
+        if (quantidade <= 0) {
+            throw new IllegalArgumentException("a quantidade deve ser maior que 0.");
+        }
+        this.produto = produto;
+        this.quantidade = quantidade;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
 }
